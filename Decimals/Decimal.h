f4442170fc4571c38d8ea1255d8fb0dc9455d5ff
Alchemik94 <alchemik94@gmail.com>
2014-04-27 18:15:41 +0200

    Operator +
    (pierwsza wersja, uwzględniony tylko ten sam znak)

--- Decimals/Decimal.h
+++ Decimals/Decimal.h
@@ -5,6 +5,8 @@
 
 #define LET(a,b) (auto a = (b))
 #define PB push_back
+#define MAX(a,b) ((a)<(b) ? (b) : (a))
+#define MIN(a,b) ((a)<(b) ? (a) : (b))
 
 using namespace std;
 
@@ -16,7 +18,25 @@ class Decimal
 	vector<ULLI> v;
 	short int sign; //short int to make it easier while multiplying or antyhing
 	const ULLI base = 1000000000; //10^9 - gives us possibility to multiply without danger of getting out of range
-
+	
+	//if any cell has bigger value than expected, we have to repair our structure and move rest to the next cell
+	void Repair()
+	{
+		for (int i = 0; i < this->v.size(); ++i)
+		if (this->v[i]>base)
+		{
+			if (i + 1 < this->v.size())
+			{
+				this->v[i + 1] += this->v[i] / base;
+				this->v[i] %= base;
+			}
+			else
+			{
+				this->v.PB(this->v[i] / base);
+				this->v[i] %= base;
+			}
+		}
+	}
 
 	public:
 		//constructors - different for signed and unsigned
@@ -68,7 +88,33 @@ class Decimal
 			return *this;
 		}
 		
+		//TODO - another signs of decimals
+		Decimal operator+(Decimal number)
+		{
+			Decimal tmp(*this);
+			int i = MIN(number.v.size(),tmp.v.size());
+			if (number.v.size() > tmp.v.size())
+			{
+				for (; i >= 0; --i)
+					number.v[i] += tmp.v[i];
+				number.Repair();
+				return number;
+			}
+			else
+			{
+				for (; i >= 0; --i)
+					tmp.v[i] += number.v[i];
+				tmp.Repair();
+				return tmp;			
+			}
+
+		}
 
 };
+
+#undef LET(a,b)
+#undef PB
+#undef MAX
+#undef MIN
 
 #endif
